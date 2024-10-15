59a6b2c93006b5deff6094029a3316dd84ef416c
minsu.lee <minsu.rob@gmail.com>
2024-10-15 21:20:53 +0900

    etc: follower follow clone start

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemeText } from "@/constants/Styles";
 import React from "react";
-import { useColorScheme } from "react-native";
+import { TouchableOpacity, useColorScheme } from "react-native";
 import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView<{ isDark: boolean }>`
@@ -50,6 +50,104 @@ const Avatar = styled.Image`
   /* left: 50%; */
 `;
 
+const Header = styled.View`
+  margin-bottom: 44px;
+  background: var(--ins-background-primary);
+  color: var(--ins-content-primary);
+  @media only screen and (max-width: 735px) {
+    display: block;
+    margin-bottom: 0px;
+  }
+`;
+const HeaderWrap = styled.View`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  column-gap: 30px;
+  @media only screen and (max-width: 735px) {
+    display: flex;
+    padding: 14px;
+    column-gap: 0px;
+  }
+`;
+const ProfilePic = styled.View`
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 735px) {
+    width: 77px;
+    height: 77px;
+    margin-right: 28px;
+  }
+`;
+const ProfileImg = styled.Image`
+  width: 150px;
+  height: 150px;
+  border-radius: 1000px;
+  border: 1px solid var(--ins-border-primary);
+  @media only screen and (max-width: 735px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+const ProfileH2 = styled(ThemeText)`
+  font-size: 28px;
+  font-weight: 300;
+  @media only screen and (max-width: 735px) {
+    display: inline-block;
+    margin-bottom: 12px;
+  }
+`;
+const ProfileIcon = styled.View`
+  margin-left: 8px;
+  @media only screen and (max-width: 735px) {
+    display: inline-block;
+  }
+`;
+const ProfileButtonWrap = styled.View`
+  margin-left: 20px;
+  @media only screen and (max-width: 735px) {
+    display: block;
+    margin-left: 0px;
+  }
+`;
+const ProfileTitle = styled.View`
+  display: flex;
+  align-items: center;
+  @media only screen and (max-width: 735px) {
+    display: block;
+  }
+`;
+
+const ProfileDescriptionH1 = styled(ThemeText)`
+  font-weight: 600;
+  line-height: 24px;
+  @media only screen and (max-width: 735px) {
+    line-height: 20px;
+  }
+`;
+const ProfileDescriptionSpan = styled.View`
+  font-weight: 400;
+  line-height: 24px;
+  @media only screen and (max-width: 735px) {
+    line-height: 20px;
+  }
+`;
+const ProfileDescriptionA = styled(ThemeText)`
+  color: var(--ins-content-blue);
+`;
+const ProfileDescriptions = styled.View`
+  @media only screen and (max-width: 735px) {
+    padding-left: 16px;
+    padding-bottom: 21px;
+    font-size: 14px;
+    margin-bottom: 0px !important;
+  }
+`;
+const ProfileRow = styled.View`
+  margin-bottom: 20px;
+`;
+
 export default function Profile({}) {
   // const { data } = useUser();
   const colorScheme = useColorScheme() ?? "light";
@@ -58,10 +156,27 @@ export default function Profile({}) {
   return (
     <Container isDark={isDark}>
       {/* <AccountIcon width="100" height="100" fill="#268596" /> */}
-      <Avatar source={require("@/assets/images/haerinBG.png")} />
-
-      <SearchTextName isDark={isDark}>이민수</SearchTextName>
-      <SearchTextCPF isDark={isDark}>다크@naver.com</SearchTextCPF>
+      {/* <Avatar source={require("@/assets/images/haerinBG.png")} /> */}
+      <HeaderWrap>
+        <ProfileRow>
+          <ProfileTitle>
+            <ProfileH2 isDark={isDark}>apple</ProfileH2>
+            <ProfileIcon></ProfileIcon>
+            <ProfileButtonWrap>
+              <TouchableOpacity>Follow</TouchableOpacity>
+            </ProfileButtonWrap>
+          </ProfileTitle>
+        </ProfileRow>
+        <ProfileRow>{/* <KeyNumbers /> */}</ProfileRow>
+        <ProfileDescriptions
+        // class="row last"
+        >
+          <ProfileDescriptionH1 isDark={isDark}>apple</ProfileDescriptionH1>
+          <ProfileDescriptionSpan>
+            Everyone has a story to tell. Tag to take part.
+          </ProfileDescriptionSpan>
+        </ProfileDescriptions>
+      </HeaderWrap>
 
       <InputArea>
         <CustomButton>
