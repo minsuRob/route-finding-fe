0377db1cd34958c463d00e502b44020324b107df
minsu.lee <minsu.rob@gmail.com>
2024-10-15 19:00:01 +0900

    fix: 기본 폰트 크기 16px, todo: 14px 16px 처리 해야할듯함.

--- constants/Styles.ts
+++ constants/Styles.ts
@@ -2,4 +2,5 @@ import styled from "styled-components/native";
 
 export const ThemeText = styled.Text<{ isDark: boolean }>`
   color: ${(props) => (props.isDark ? "#FFF" : "#000")};
+  font-size: 16px;
 `;
