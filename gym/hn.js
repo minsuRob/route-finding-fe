780b7e593cae5b4c1976cf98f319f0b32d7ce985
minsu.lee <minsu.rob@gmail.com>
2024-10-30 00:34:54 +0900

    etc: add comment

--- gym/hn.js
+++ gym/hn.js
@@ -326,3 +326,19 @@ const searchQuerys = [
 })()
   .catch((err) => console.error(err))
   .finally(() => browser.close());
+
+/*
+
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.google.com");
+  const title = await page.title();
+  const bodyHTML = await page.evaluate(() => document.body.innerHTML);
+  console.log("Title:", title);
+  console.log("Body:", bodyHTML);
+  await browser.close();
+})();
+*/
