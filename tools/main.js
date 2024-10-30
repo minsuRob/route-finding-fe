1452298274a12c7c420996a2f77ac419781a8914
minsu.lee <minsu.rob@gmail.com>
2024-10-31 00:51:37 +0900

    fix: change the sheet id

--- tools/main.js
+++ tools/main.js
@@ -8,7 +8,7 @@ async function main() {
 
     // 아래와 같은 구글 스프레드시트 주소 중 d와 edit 사이에 들어있는 부분이 스프레드시트의 ID 값 입니다.
     // https://docs.google.com/document/d/1bZbLi45kqRyE1fSBphWzFFKaJobcaMplBzr82rRXjPM/edit#
-    const spreadsheetId = '1z2d4gBO8JSI8SEotnHDKdcq8EQ9X4O5fWPxeUCAqW1c';
+    const spreadsheetId = "1mTrUcG0LuF9YvWezVXrIjmliw3oA3R2K";
 
     const notice = await downloader.downloadToJson(
       spreadsheetId,
