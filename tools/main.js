0b3c01bf0a71834081038be9ef3824e080dbd7b3
minsu.lee <minsu.rob@gmail.com>
2024-10-31 00:53:01 +0900

    etc: fix syntax

--- tools/main.js
+++ tools/main.js
@@ -1,5 +1,5 @@
-const SheetApiClientFactory = require('./sheet_api_client_factory');
-const SheetDownloader = require('./sheet_downloader');
+const SheetApiClientFactory = require("./sheet_api_client_factory");
+const SheetDownloader = require("./sheet_downloader");
 
 async function main() {
   try {
@@ -12,16 +12,16 @@ async function main() {
 
     const notice = await downloader.downloadToJson(
       spreadsheetId,
-      'notice',
-      'downloaded/notice.json',
+      "notice",
+      "downloaded/notice.json"
     );
 
     console.log(notice);
 
     const countryInfo = await downloader.downloadToJson(
       spreadsheetId,
-      'countryInfo',
-      'downloaded/countryInfo.json',
+      "countryInfo",
+      "downloaded/countryInfo.json"
     );
 
     console.log(countryInfo);
