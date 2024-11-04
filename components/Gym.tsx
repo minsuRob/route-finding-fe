4d86a2287f4462be80a8cf609c0865d9e7b4d232
minsu.lee <minsu.rob@gmail.com>
2024-11-04 21:36:54 +0900

    etc: gym 상하단 여백 추가

--- components/Gym.tsx
+++ components/Gym.tsx
@@ -1,9 +1,15 @@
-import { useColorScheme } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
+import { TouchableOpacity, useColorScheme } from "react-native";
 import styled from "styled-components/native";
 const colorScheme = useColorScheme() ?? "light";
 const isDark = colorScheme === "dark" ? true : false;
 
-const Gym: React.FC<GymItemProps> = ({ gym_name }) => {
+const Gym: React.FC<GymItemProps> = ({ gym_name, address }) => {
+  const goToProfile = () => {
+    router.push("/GymInfo");
+  };
+
   return (
     <Container>
       <Header>
@@ -11,7 +17,9 @@ const Gym: React.FC<GymItemProps> = ({ gym_name }) => {
           resizeMode="cover"
           source={require("@/assets/images/haerin.jpg")}
         />
-        <Username isDark={isDark}>{gym_name}</Username>
+        <Username onPress={goToProfile} isDark={isDark}>
+          {gym_name}
+        </Username>
       </Header>
       {/* <File
           resizeMode="cover"
@@ -19,6 +27,35 @@ const Gym: React.FC<GymItemProps> = ({ gym_name }) => {
   
           // source={{ uri: imgfile[id] }}
         /> */}
+      <ExtraContainer>
+        <Actions>
+          <Action>
+            <Ionicons
+              name={"heart-outline"}
+              // name={isLiked ? "heart" : "heart-outline"}
+              // color={isLiked ? "tomato" : "white"}
+              color={"white"}
+              size={22}
+            />
+          </Action>
+          <Action>
+            <Ionicons name="chatbubble-outline" color="white" size={22} />
+          </Action>
+        </Actions>
+        <TouchableOpacity
+          onPress={() => {
+            // navigation.navigate("Likes", { photoId: id });
+          }}
+        >
+          {/* <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes> */}
+        </TouchableOpacity>
+        <Caption>
+          <TouchableOpacity onPress={goToProfile}>
+            <Username isDark={isDark}>{gym_name}</Username>
+          </TouchableOpacity>
+          <CaptionText isDark={isDark}>{address}</CaptionText>
+        </Caption>
+      </ExtraContainer>
     </Container>
   );
 };
@@ -39,13 +76,27 @@ const Username = styled.Text<{ isDark: boolean }>`
   color: ${(props) => (props.isDark ? "#FFF" : "#000")};
 `;
 const File = styled.Image``;
-const Actions = styled.View``;
-const Action = styled.TouchableOpacity``;
-const Caption = styled.View``;
-const CaptionText = styled.Text``;
+const Actions = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const Action = styled.TouchableOpacity`
+  margin-right: 10px;
+`;
+const Caption = styled.View`
+  flex-direction: row;
+`;
+const CaptionText = styled.Text<{ isDark: boolean }>`
+  color: ${(props) => (props.isDark ? "#FFF" : "#000")};
+  margin-left: 5px;
+`;
 const Likes = styled.Text``;
 
-const ExtraContainer = styled.View``;
+const ExtraContainer = styled.View`
+  padding: 10px;
+  border-bottom-width: 2px;
+  border-color: aqua;
+`;
 
 export default Gym;
 
