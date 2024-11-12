4a60427f6b6de65cb75aea5980f32aacec9ca7bd
minsu.lee <minsu.rob@gmail.com>
2024-11-12 23:26:20 +0900

    etc: fix index layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Screen } from "@/ignite/Screen";
 import { ThemedText } from "@/components/ThemedText";
 import { translate } from "@/constants/i18n";
 import { ThemeText } from "@/constants/Styles";
@@ -12,70 +13,6 @@ export const Container = styled.SafeAreaView<{ isDark: boolean }>`
   align-items: center;
 `;
 
-const Avatar = styled.Image`
-  width: 100px;
-  height: 100px;
-  border-radius: 80%;
-  border: 5px solid #fff;
-  /* position: absolute; */
-  /* top: 100%; */
-  /* left: 50%; */
-`;
-
-// interface MyProfileProps {
-//   navigation: NavigationProp<RootSharedStackParamList, "Profile">;
-// }
-
-//////////
-
-const ButtonBox = styled.View`
-  width: 100%;
-`;
-
-const UserInfoBox = styled.View`
-  margin: 10px 0;
-  align-items: center;
-`;
-
-const Username = styled.Text`
-  font-weight: bold;
-  font-size: 25px;
-  color: ${(props) => props.theme.accent};
-`;
-
-const Name = styled.Text`
-  font-weight: bold;
-  font-size: 18px;
-  color: ${(props) => props.theme.grayColor};
-`;
-
-const Email = styled.Text`
-  color: ${(props) => props.theme.grayColor};
-  margin-bottom: 10px;
-`;
-
-const FollowContainer = styled.View`
-  max-width: 500px;
-  width: 90%;
-  padding: 8px 0;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-around;
-  border: 1px solid ${(props) => props.theme.accent};
-  border-radius: 5px;
-`;
-
-const FollowText = styled.Text`
-  text-align: center;
-  font-weight: bold;
-  color: ${(props) => props.theme.fontColor};
-`;
-
-const FollowValue = styled.Text`
-  text-align: center;
-  color: ${(props) => props.theme.fontColor};
-`;
-
 const FollowContentsBox = styled.TouchableOpacity`
   width: 20%;
   border-color: ${(props) => props.theme.accent};
@@ -88,38 +25,13 @@ export default function Profile({}) {
   // <Avatar source={require("@/assets/images/haerinBG.png")} />
 
   return (
-    <Container isDark={isDark}>
-      {/* <AccountIcon width="100" height="100" fill="#268596" /> */}
-
-      <FollowContainer>
-        <Avatar source={require("@/assets/images/haerinBG.png")} />
-        <FollowContentsBox
-          style={{ borderRightWidth: 1, borderStyle: "solid" }}
-        >
-          <FollowText>{translate("profile.startedAt")}</FollowText>
-
-          <FollowValue>200 🗓️</FollowValue>
-        </FollowContentsBox>
-        <FollowContentsBox
-          style={{ borderRightWidth: 1, borderStyle: "solid" }}
-        >
-          <FollowText>{translate("profile.followers")}</FollowText>
-          <FollowValue>200</FollowValue>
-        </FollowContentsBox>
-        <FollowContentsBox>
-          <FollowText>{translate("profile.following")}</FollowText>
-          <FollowValue>100</FollowValue>
-        </FollowContentsBox>
-      </FollowContainer>
-      <UserInfoBox>
-        <Username>vanes kang</Username>
-        <Name>{translate("welcomeScreen.exciting")}</Name>
-        <Email>hrkang@gmail.com</Email>
-        <ButtonBox>{/* <Button text={"프로필 수정하기"} /> */}</ButtonBox>
-        <ButtonBox style={{ marginTop: 5 }}>
-          {/* <Button redBgColor={true} text={"로그아웃"} /> */}
-        </ButtonBox>
-      </UserInfoBox>
-    </Container>
+    <Screen preset="fixed">
+      <Container isDark={isDark}>
+        {/* <AccountIcon width="100" height="100" fill="#268596" /> */}
+        <ThemedText type="title">Welcome!!</ThemedText>
+        <ThemedText type="title">Welcome!!</ThemedText>
+        <ThemedText type="title">Welcome!!</ThemedText>
+      </Container>
+    </Screen>
   );
 }
